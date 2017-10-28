3e51291959b30bc5a73c16355bb211487900a94a
rJankowski93 <rafal.jankowski.93@gmail.com>
2017-10-28 23:37:10 +0200

    event

--- app/view/Event.js
+++ app/view/Event.js
@@ -1,32 +1,51 @@
 import React from "react";
-import {StyleSheet, Text, View, Image, Linking, Component} from "react-native";
+import {StyleSheet, Text, View, Image, Linking, Component, TouchableHighlight} from "react-native";
 import MapView from "react-native-maps";
-import Button from "react-native-button";
+
+export const CATEGORY = {
+    FIRE: 'Fire',
+    CAT: 'Cat',
+    TREE: 'Tree',
+    ACCIDENT_CAR: 'Accident car',
+};
+
+export const PRIORITY = {
+    HIGH: 'High',
+    NORMAL: 'Normal',
+    LOW: 'Low',
+};
+
+const event = {
+    name: 'New event',
+    description: 'new event very very busy day',
+    category: CATEGORY.FIRE,
+    priority: PRIORITY.NORMAL,
+    latitude: '50.12180069999999',
+    longitude: '19.020002299999987'
+};
 
 export default class Event extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { pressStatus: false };
+    }
+    onHideUnderlay(){
+        this.setState({ pressStatus: false });
+    }
+    onShowUnderlay(){
+        this.setState({ pressStatus: true });
+    }
+
     render() {
-        var MOCKED_EVENTS_DATA = [
-            {
-                name: 'New event',
-                description: 'new event very very busy day',
-                category: CATEGORY.FIRE,
-                priority: PRIORITY.NORMAL,
-                latitude: '50.12180069999999',
-                longitude: '19.020002299999987'
-            },
-        ];
-        var event = MOCKED_EVENTS_DATA[0];
-
         return (
-
             <View style={styles.container}>
 
                 <View style={styles.text}>
-                    <Text>Nazwa: {event.name}</Text>
-                    <Text>Opis: {event.description}</Text>
-                    <Text>Kategoria: {event.category}</Text>
-                    <Text>Priorytet: {event.priority}</Text>
+                    <Text>  <Text style={styles.names}>Nazwa: </Text> {event.name} </Text>
+                    <Text>  <Text style={styles.names}>Opis: </Text> {event.description} </Text>
+                    <Text>  <Text style={styles.names}>Kategoria: </Text> {event.category} </Text>
+                    <Text>  <Text style={styles.names}>Priorytet: </Text> {event.priority} </Text>
                 </View>
                 <MapView
                     style={styles.map}
@@ -38,15 +57,16 @@ export default class Event extends React.Component {
                     }}
                 />
                 <View style={styles.buttons}>
-                    <Button style={styles.buttonAccept}
-                            onPress={() => this.acceptEvent(event.latitude,event.longitude)}>
-
-                        Akceptuj
-                    </Button>
-                    <Button style={styles.buttonReject}
-                            onPress={() => this.rejectEvent()}>
-                        Odrzuc
-                    </Button>
+                    <TouchableHighlight style={this.state.pressStatus ? styles.buttonAcceptPress : styles.buttonAccept }
+                                        onPress={() => this.acceptEvent(event.latitude, event.longitude)}
+                                        onHideUnderlay={this.onHideUnderlay.bind(this)}
+                                        onShowUnderlay={this.onShowUnderlay.bind(this)}>
+                        <Text style={styles.buttonAcceptText}>Akceptuj</Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight style={styles.buttonReject}
+                                        onPress={() => this.rejectEvent()}>
+                        <Text style={styles.buttonRejectText}>Odrzuć</Text>
+                    </TouchableHighlight>
                 </View>
             </View>
 
@@ -70,25 +90,61 @@ var styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#C2C2C2',
     },
+    names:{
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
     text: {
         flex: 1,
         marginTop: 50,
     },
     map: {
-        width: 300,
+        width: 400,
         flex: 3,
     },
     buttonAccept: {
-        backgroundColor: '#6aff29',
         width: 100,
         height: 50,
-        margin: 10,
+        marginRight:40,
+        marginLeft:40,
+        marginTop:10,
+        paddingTop:15,
+        backgroundColor:'#88ff80',
+        borderRadius:10,
+        borderWidth: 1,
+        borderColor: '#000000'
+    },
+    buttonAcceptPress:{
+        width: 100,
+        height: 50,
+        marginRight:40,
+        marginLeft:40,
+        marginTop:10,
+        paddingTop:15,
+        backgroundColor:'#6aff29',
+        borderRadius:10,
+        borderWidth: 1,
+        borderColor: '#000000'
+    },
+    buttonAcceptText: {
+        color:'#000000',
+        textAlign:'center',
     },
     buttonReject: {
-        backgroundColor: '#ff4b48',
         width: 100,
         height: 50,
-        margin: 10,
+        marginRight:40,
+        marginLeft:40,
+        marginTop:10,
+        paddingTop:15,
+        backgroundColor:'#ff2f3e',
+        borderRadius:10,
+        borderWidth: 1,
+        borderColor: '#000000'
+    },
+    buttonRejectText: {
+        color:'#000000',
+        textAlign:'center',
     },
     buttons: {
         flexDirection: 'row',
@@ -97,15 +153,4 @@ var styles = StyleSheet.create({
     },
 });
 
-export const CATEGORY = {
-    FIRE: 'Fire',
-    CAT: 'Cat',
-    TREE: 'Tree',
-    ACCIDENT_CAR: 'Accident car',
-};
 
-export const PRIORITY = {
-    HIGH: 'High',
-    NORMAL: 'Normal',
-    LOW: 'Low',
-};
