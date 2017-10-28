81f3f00dc24e2cd8754362cc13d21d2eb4e72bd5
Karol Klimończyk <k.klimonczyk@gmail.com>
2017-10-28 19:22:11 +0200

    -added current cords and marker

--- app/view/EventCreationView.js
+++ app/view/EventCreationView.js
@@ -2,19 +2,79 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import MapView from 'react-native-maps';
 
+const delta = {
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421
+};
+
 export default class EventCreationView extends React.Component {
+
+    constructor() {
+        super();
+
+        //set region and markers
+        this.state = {
+            region: {
+                latitude: 19.9449799,
+                longitude: 50.0646501,
+                latitudeDelta: delta.latitudeDelta,
+                longitudeDelta: delta.longitudeDelta
+            },
+            error: null,
+            markers: [
+                {
+                    id: 1,
+                    latlng:{
+                        latitude: 19.9449799,
+                        longitude: 50.0646501
+                    },
+                    title: 'Foo Place',
+                    description: '1234 Foo Drive'
+                }
+            ]
+        };
+    }
+
+    //get current cords after creating component
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({
+                    region: {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude,
+                        latitudeDelta: delta.latitudeDelta,
+                        longitudeDelta: delta.longitudeDelta
+                    }
+                },()=>this.forceUpdate());
+
+            },
+            (error) => this.setState({error: error.message}),
+            {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+        );
+    }
+
     render() {
         return (
-            <View >
+            <View>
                 <MapView
                     style={styles.map}
-                    initialRegion={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
-                    }}
-                />
+                    region={this.state.region}
+                    showsMyLocationButton={true}
+                    onRegionChange={ region => this.setState({region}) }
+                    onRegionChangeComplete={ region => this.setState({region}) }
+                   >
+
+                    {this.state.markers.map(marker => (
+                        <MapView.Marker
+                            key={marker.id}
+                            coordinate={marker.latlng}
+                            title={marker.title}
+                            description={marker.description}
+                        />
+                    ))}
+
+                </MapView>
             </View>
         );
     }
